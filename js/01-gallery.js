d6cc2f2e763e608f03aa51a74289d4b7fc430e08
iSIayer <gamerspeed86@gmail.com>
2022-05-23 00:57:48 +0300

    final hw-07

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,85 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+document.body.style.backgroundColor = "black";
 
+const refs = {
+  gallery: document.querySelector(".gallery"),
+  galleryImages: document.querySelectorAll(".gallery__image"),
+};
+
+// Сreating photo-card layouts
+function createPhotoCardMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      loading="lazy"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
+// Render photo-card layouts
+refs.gallery.insertAdjacentHTML(
+  "beforeend",
+  createPhotoCardMarkup(galleryItems)
+);
+
+// Add event listener to all gallery items
+refs.gallery.addEventListener("click", handleClick);
+
+// Create function open-close modal window
+function handleClick(e) {
+  e.preventDefault();
+  const isGalleryItem = e.target.classList.contains("gallery__image");
+  if (!isGalleryItem) {
+    return;
+  } else {
+    const instance = basicLightbox.create(
+      `<img src="${e.target.dataset.source}" alt="${e.target.alt}">`,
+      {
+        onShow: () => {
+          window.addEventListener("keydown", onEscape);
+        },
+      }
+    );
+
+    instance.show();
+
+    function onEscape(e) {
+      if (e.key === "Escape") {
+        instance.close();
+        window.removeEventListener("keydown", onEscape);
+      }
+    }
+    console.log(e.target);
+  }
+}
+
+// Control lazy loading
+if ("loading" in HTMLImageElement.prototype) {
+  refs.galleryImages.forEach((image) => {
+    image.setAttribute("loading", "lazy");
+  });
+} else {
+  createLazyLoadEl();
+}
+
+function createLazyLoadEl() {
+  const script = document.createElement("script");
+  script.src =
+    "https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js";
+  script.integrity =
+    "sha512-q583ppKrCRc7N5O0n2nzUiJ+suUv7Et1JGels4bXOaMFQcamPk9HjdUknZuuFjBNs7tsMuadge5k9RzdmO+1GQ==";
+  script.crossOrigin = "anonymous";
+  script.referrerPolicy = "no-referrer";
+
+  document.body.appendChild(script);
+}
 console.log(galleryItems);
--- js/gallery-items.js
+++ js/gallery-items.js
@@ -1,65 +1,107 @@
 export const galleryItems = [
   {
     preview:
-      'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825__340.jpg',
+      "https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825__340.jpg",
     original:
-      'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg',
-    description: 'Hokkaido Flower',
+      "https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg",
+    description: "Hokkaido Flower",
   },
   {
     preview:
-      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
+      "https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg",
     original:
-      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
-    description: 'Container Haulage Freight',
+      "https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg",
+    description: "Container Haulage Freight",
   },
   {
     preview:
-      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
+      "https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg",
     original:
-      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
-    description: 'Aerial Beach View',
+      "https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg",
+    description: "Aerial Beach View",
   },
   {
     preview:
-      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
+      "https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg",
     original:
-      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
-    description: 'Flower Blooms',
+      "https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg",
+    description: "Flower Blooms",
   },
   {
     preview:
-      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
+      "https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg",
     original:
-      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
-    description: 'Alpine Mountains',
+      "https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg",
+    description: "Alpine Mountains",
   },
   {
     preview:
-      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
+      "https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg",
     original:
-      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
-    description: 'Mountain Lake Sailing',
+      "https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg",
+    description: "Mountain Lake Sailing",
   },
   {
     preview:
-      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
+      "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg",
     original:
-      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
-    description: 'Alpine Spring Meadows',
+      "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg",
+    description: "Alpine Spring Meadows",
   },
   {
     preview:
-      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
+      "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg",
     original:
-      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
-    description: 'Nature Landscape',
+      "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg",
+    description: "Nature Landscape",
   },
   {
     preview:
-      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
+      "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg",
     original:
-      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
-    description: 'Lighthouse Coast Sea',
+      "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",
+    description: "Lighthouse Coast Sea",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2020/07/04/20/12/green-lizard-5370821__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2020/07/04/20/12/green-lizard-5370821_960_720.jpg",
+    description: "Green Lizard",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2022/04/21/19/47/lion-7148207__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2022/04/21/19/47/lion-7148207_960_720.jpg",
+    description: "Lion",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2022/05/16/18/17/sheep-7200918__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2022/05/16/18/17/sheep-7200918_960_720.jpg",
+    description: "Sheeps",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2021/07/24/01/54/macaw-6488488__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2021/07/24/01/54/macaw-6488488_960_720.jpg",
+    description: "Macaw Ara",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2022/04/27/07/47/bee-7159876__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2022/04/27/07/47/bee-7159876_960_720.jpg",
+    description: "Bee in flower",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2022/05/01/12/36/great-spotted-woodpecker-7167422__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2022/05/01/12/36/great-spotted-woodpecker-7167422_960_720.jpg",
+    description: "Great Spotted Woodpecker",
   },
 ];
